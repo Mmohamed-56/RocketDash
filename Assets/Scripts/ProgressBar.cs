0a743720b472fdd8dcc406047c923c8323e284dd
Magdy <mags5608@gmail.com>
2025-11-17 20:13:17 -0500

    Added progress bar, conclude project with final build (Subject to more Updates).

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -3,30 +3,28 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ProgressBar : MonoBehaviour{
+    
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform landingPad;
+    [SerializeField] private Slider progressBar;
 
-   public Slider slider;
-    [SerializeField] private float fillSpeed = 0.5f;
-    [SerializeField] private float targetProgress = 0f;
+    private float startX;
+    private float endX;
 
-
-    public void Update()
+    void Start()
     {
-        
-    }
-    public void incrementProgress(float amount){
-        targetProgress += amount;
-        if (targetProgress > 1f){
-            targetProgress = 1f;
-        }
-        if (targetProgress < 0f){
-            targetProgress = 0f;
-        }
+        startX = player.position.x;
+        endX = landingPad.position.x;
 
-        while(slider.value < targetProgress){
-            slider.value += fillSpeed * Time.deltaTime;
-            if (slider.value > targetProgress){
-                slider.value = targetProgress;
-            }
-        }
+        progressBar.value = 0f;
+        progressBar.maxValue = endX - startX;
     }
+
+
+    private void Update()
+    {
+        float distanceCovered = player.position.x - startX;
+        progressBar.value = Mathf.Clamp(distanceCovered, 0, progressBar.maxValue);
+    }
+      
 }
